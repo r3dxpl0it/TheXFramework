a277cfff709c85df2556bab3f39a41fc78dc0212
Jim Was Here [a.k.a R3dxpl0it] <return_root@protonmail.com>
2018-12-05 10:51:55 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python python-dev python-all python-all-dev 
+sudo apt-get install python3-dev python3-pip libxslt1-dev libxml2-dev python3 zlib1g-dev
+sudo apt-get nmap golismero sql-map 
+unzip data.zip
+cd Modules/ && unzip archive.zip
+cd ..
+pip install -r requirements.txt
+pip3 install -r requirements.txt
