e3498cbaf09a44fd3258d1f7b71572b700058412
r3dxpl0it <root@localhost.localdomain>
2018-12-06 01:24:40 -0500

    delete

--- Modules/Photon/core/config.py
+++
@@ -1,2 +0,0 @@
-intels = ['github.com', 'facebook.com', 'instagram.com', 'youtube.com']
-badTypes = ['png', 'jpg', 'jpeg', 'pdf', 'js', 'css', 'ico', 'bmp', 'svg', 'json', 'xml', 'xls', 'csv', 'docx']
--- Modules/Photon/plugins/exporter.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def exporter(directory, method, datasets):
-	if method.lower() == 'json':
-		json_string = json.dumps(datasets, indent=4) # convert json_dict to a json styled string
-		savefile = open('%s/exported.json' % directory, 'w+') # save
-		savefile.write(json_string)
-		savefile.close()
