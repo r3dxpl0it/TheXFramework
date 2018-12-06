781977ec6e40216fd143909afc04640788767290
Jim Was Here [a.k.a R3dxpl0it] <return_root@protonmail.com>
2018-12-06 18:28:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,31 @@
+echo "......................................................"
+echo "THE X FRAMWORK INSTALLER"
+echo "UPDATING AND INSTALLING LIBRARIES, MODULES AND DEPENDENCIES"
+echo "......................................................"
+echo ""
+echo "......................................................"
+echo "Updating"
+echo "......................................................"
+sudo apt-get update 
+echo "......................................................"
+echo "Upgrading"
+echo "......................................................"
+sudo apt-get upgrade
+echo "......................................................"
+echo "Installing Libraries"
+echo "......................................................"
+sudo apt-get install libxml2 libxslt 
 sudo apt-get install python python-dev python-all python-all-dev 
 sudo apt-get install python3-dev python3-pip libxslt1-dev libxml2-dev python3 zlib1g-dev
+echo "......................................................"
+echo "Installing Modules"
+echo "......................................................"
 sudo apt-get nmap golismero sql-map 
-unzip data.zip
-cd Modules/ && unzip archive.zip
-cd ..
+echo "......................................................"
+echo "Installing Python2 Modules" 
+echo "......................................................"
 pip install -r requirements.txt
+echo "......................................................"
+echo "Installing Python3 Modules" 
+echo "......................................................"
 pip3 install -r requirements.txt
